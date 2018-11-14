62cbca3886130f2ae0ce9da6aa3c239c06b92b9e
Guanhua Ding <guanhuading@Guanhuas-MacBook-Pro-2018.local>
2018-11-14 22:25:29 +1000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -7,8 +7,29 @@
 
 // console.log("User flipped " + cardTwo);
 // console.log("User flipped " + cardFour);
+var cards = [
+{
+rank: 'queen',
+suit: 'hearts',
+cardImage: 'images/queen-of-hearts.png'
+},
+{
+rank: 'queen',
+suit: 'diamonds',
+cardImage: 'images/queen-of-diamonds.png'
+},
+{
+rank: 'king',
+suit: 'hearts',
+cardImage: 'images/king-of-hearts.png'
+},
+{
+rank: 'king',
+suit: 'diamonds',
+cardImage: 'images/king-of-diamonds.png'
+}
+];
 
-var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
 function checkForMatch(){
@@ -20,8 +41,10 @@ function checkForMatch(){
 }
 
 function flipCard(cardId){
-	cardsInPlay.push(cards[cardId])
-	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}
